a20f85c1fd84c7dd0b59a81da79bad5afab1abe9
Chih-Kang Huang <asddsa155@gmail.com>
2023-04-12 00:35:54 +0800

    update

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ restaurants = [
     '華圓', '寶多福', '小林', '月枱', '呂媽媽',
     '佳臻', '小煮角', '中一排骨', '田園小轆', '能量小姐',
     '開心越南', '兄弟', '榮興', '簡單', '好味', '華園',
-    '全樂', '高又鮮'
+    '全樂', '高又鮮', 
 ]
 # beverages = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet=beverages")['name'].to_list()
 beverages = [
